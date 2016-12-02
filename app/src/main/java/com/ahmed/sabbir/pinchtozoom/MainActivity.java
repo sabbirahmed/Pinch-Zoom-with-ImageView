d4018d01848af0a468fa9ce2967631327e118c8b
Sabbir Ahmed <sabbir.cn@gmail.com>
2016-12-02 17:55:34 +0600

    first commit

---
+++ app/src/main/java/com/ahmed/sabbir/pinchtozoom/MainActivity.java
@@ -0,0 +1,22 @@
+package com.ahmed.sabbir.pinchtozoom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import uk.co.senab.photoview.PhotoViewAttacher;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        PhotoViewAttacher photoViewAttacher = new PhotoViewAttacher(imageView);
+        photoViewAttacher.update();
+
+    }
+}
